10b727decf5a225111e527a452867d4a8ec62deb
khanh2005fpt <nguyenhuubaokhanh2005@gmail.com>
2025-12-02 19:22:50 +0700

    first commit

---
+++ lmms-backend/lmms-backend/src/main/java/net/javaguides/lmms/LmmsBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.lmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmmsBackendApplication.class, args);
+	}
+
+}
---
+++ lmms-backend/lmms-backend/src/test/java/net/javaguides/lmms/LmmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.lmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
